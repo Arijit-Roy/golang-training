0b1460438fc4cb30dfda79e008c68b70699cc6a0
arijit-roy <arijit.roy@hotmail.co.uk>
2018-08-03 15:50:46 +0530

    Golang Training- Structures and pointers

---
+++ STRUCT/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+)
+
+type person struct {
+	firstName string
+	lastName string
+	contactInfo
+}
+
+type contactInfo struct {
+	email string
+	zipCode int
+}
+
+func main() {
+	// alex := person{"Alex", "Anderson"}
+
+	// alex := person{ firstName: "Alex", lastName: "Anderson"}
+	// fmt.Println(alex)
+
+	// var james person
+	// fmt.Println(james)
+	// james.firstName = "Arijit"
+	// james.lastName = "Roy"
+	// // %+v prints out all the fildnames and corresponding values in a struct
+	// fmt.Printf("%+v", james);
+
+	jim := person{
+		firstName: "Jim",
+		lastName: "Party",
+		contactInfo: contactInfo{
+			email: "jim@gmail.com",
+			zipCode: 4567732,
+		},
+	}
+	// jimPointer := &jim;
+	// jimPointer.updateName("Jimmy")
+	jim.updateName("Jimmy")
+	jim.print()
+	
+}
+func (personPointer *person) updateName(newFirstName string){
+	(*personPointer).firstName = newFirstName
+}
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
